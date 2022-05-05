ad2a9df57dbe1a0d9c98e07bce70e47c5f8effc9
Muhammad Zubair <72731372+Zuba112@users.noreply.github.com>
2022-05-05 10:17:59 +0100

    Finished the GUI.

---
+++ Application Design/whereami.m
@@ -0,0 +1,122 @@
+function varargout=whereami(varargin)
+%WHEREAMI Automatic geolocation and "you are here!" map
+%	WHEREAMI tries to locate where your computer is and produces a map of
+%	illuminated relief topography from SRTM data as new figure, centered on
+%	your position. The function needs an internet connection, READHGT and 
+%	DEM functions both available at https://github/IPGP/mapping-matlab
+%
+%	WHEREAMI(HOST) tries to geolocate the host name or IPv4 address HOST.
+%
+%	WHEREAMI([LAT LON]) fixes the latitude LAT and longitude LON values
+%	(must be a 2-element vector).
+%
+%	WHEREAMI(...,DLAT) fixes the latitude length DLAT (map's height) in 
+%	degree (default is 1, i.e., around 111 km).
+%
+%	WHEREAMI(...,param1,param2,...) will use any additional arguments 
+%	transmitted to the DEM function to adjust the map rendering. See DEM 
+%	documentation for available options and details.
+%
+%	X=WHEREAMI(...) returns a structure X with fields:
+%	   dlat: DLAT value
+%	   host: HOST string
+%	    lat: LAT value
+%	    lon: LON value
+%	  query: structure as returned by the geolocation request
+%
+%	Examples:
+%	   >> whereami
+%	   will make a figure centered on your present position on Earth 
+%	  (guessed from you computer internet address).
+%
+%	   >> whereami('www.ipgp.fr')
+%	   will make a figure centered on the IPGP institute in Paris, France.
+%
+%	   >> whereami([53.38 13.06],0.5)
+%	   will make a figure centered on latitude/longitude 53.38°N / 13.06°E,
+%	   which is the GFZ institute in Potsdam, Germany, with a 0.5° height.
+%
+%
+%	Author: F. Beauducel <beauducel@ipgp.fr>
+%	Created: 2013-01-05
+%	Updated: 2021-12-09
+
+
+X.dlat = 1;
+api = 'http://ip-api.com/json/';
+
+if nargin < 1
+	X.host = '';
+elseif ischar(varargin{1})
+	X.host = varargin{1};
+elseif isnumeric(varargin{1})
+	if numel(varargin{1}) == 2
+		X.lat = varargin{1}(1);
+		X.lon = varargin{1}(2);
+	else
+		X.dlat = varargin{1}(1);
+	end
+end
+
+demopt = {};	
+if nargin > 1
+	if isnumeric(varargin{2}) && isscalar(varargin{2})
+		X.dlat = varargin{2};
+		if nargin > 2
+			demopt = varargin(3:end);
+		end
+	else
+		demopt = varargin(2:end);
+	end
+end
+
+% geolocation from IP
+if ~isfield(X,'lat')
+	url = [api X.host];
+	if exist('webread','file') == 2
+		D = webread(url);
+		if ~isstruct(D) % for GNU Octave compatibility
+			D = json2struct(D);
+		end
+	else % backward compatibility for Matlab < 2014b
+		D = json2struct(urlread(url,'TimeOut',5));
+	end
+	if ~isfield(D,'lat') || ~isfield(D,'lon')
+		error('%s cannot be geolocated.',X.host)
+	else
+		X.lat = D.lat;
+		X.lon = D.lon;
+		X.query = D;
+	end
+end
+
+if nargout > 0
+	varargout{1} = X;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function J=json2struct(json)
+
+C = textscan(json,'%s','delimiter',',');
+for n = 1:length(C{1})
+	s = textscan(regexprep(C{1}{n},'[{}]',''),'%s','Delimiter',':');
+	key = regexprep(s{1}{1},'[".]',''); % removes any double quote or dot
+	if length(s{1}) > 1
+		if ~isempty(strfind(s{1}{2},'"'))
+			val = regexprep(s{1}{2},'"','');
+		else
+			val = str2double(s{1}{2});
+		end
+		J.(key) = val;
+	end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function target(x,y)
+
+m = 'o';
+s = 8;
+hold on
+plot(x,y,m,'MarkerSize',s,'MarkerFaceColor',[1,0,0],'MarkerEdgeColor',.2*ones(1,3),'Linewidth',.5);
+plot(x,y,m,'MarkerSize',s+2,'MarkerEdgeColor',.99*ones(1,3),'MarkerFaceColor','none');
+hold off
